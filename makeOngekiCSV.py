8cca4b790fadf2a2744b0f082f14533150f4437d
kirisakku <kirisakku@yahoo.co.jp>
2021-10-25 00:14:23 +0900

    2021/10/25時点のWikiの形式に合わせてツールを修正

--- makeOngekiCSV.py
+++ makeOngekiCSV.py
@@ -11,15 +11,9 @@ for i in range(len(textlist)):
     dataText = textlist[i].replace(",", "").replace(", "," ").replace("，", " ")
     # スペースで区切りリストを作る
     datalist = dataText.split("	")
-    # 先頭に難易度種類追加
-    if "MAS" in datalist[0]:
-        datalist.insert(0, 'MASTER')
-    elif "EXP" in datalist[0]:
-        datalist.insert(0, 'EXPERT')
     # 不要なデータの削除
-    del datalist[1]
-    del datalist[7:9]
-    del datalist[3:6]
+    del datalist[2]
+    del datalist[3:5]
     # カンマでくっつける
     csv = ",".join(datalist)
     # 結果を格納
